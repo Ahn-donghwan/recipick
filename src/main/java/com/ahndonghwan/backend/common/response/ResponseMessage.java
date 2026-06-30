b376ed8a1f73413a014f16a3268f8eadc1254573
Ahn DongHwan <ddh1713flrwls1713@gmail.com>
2026-06-30 13:40:09 +0900

    common response settings

---
+++ src/main/java/com/ahndonghwan/backend/common/response/ResponseMessage.java
@@ -0,0 +1,15 @@
+package com.ahndonghwan.backend.common.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ResponseMessage {
+
+    SUCCESS_TO_CREATE_PRODUCT("상품 생성 성공"),
+    SUCCESS_TO_GET_PRODUCT_DETAIL("상품 상세정보 조회 성공"),
+    ;
+
+    private final String message;
+}
