5d0e1ab350c26006f6ca3affdc737f5c64495a51
Ahn DongHwan <ddh1713flrwls1713@gmail.com>
2026-07-02 13:31:07 +0900

    feat(batch): 회원 대량 생성 배치 작업 초기화

--- src/main/java/com/ahndonghwan/backend/batch/member/MemberBulkJobConfig.java
+++ src/main/java/com/ahndonghwan/backend/batch/member/MemberBulkJobConfig.java
@@ -1,4 +1,13 @@
 package com.ahndonghwan.backend.batch.member;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MemberBulkJobConfig {
+
+    // 수동 빈 등록
+
+    // 의존성 주입
+
+    // Scheduler
 }
